87f7a9219409cbbad75ee997f7fb020e836dffb3
Remco Haszing <remcohaszing@gmail.com>
2024-04-16 12:19:06 +0200

    Refactor project layout
    - Leverage project references
    - Replace hacks in the demo example
    - Publish source maps
    - Publish declaration maps

--- examples/demo/webpack.config.js
+++ examples/demo/webpack.config.js
@@ -1,48 +1,39 @@
-import { fileURLToPath } from 'url'
-
 import CssMinimizerPlugin from 'css-minimizer-webpack-plugin'
 import HtmlWebPackPlugin from 'html-webpack-plugin'
 import MiniCssExtractPlugin from 'mini-css-extract-plugin'
 
-const monacoUnified = new URL('../../', import.meta.url)
-const nodeModules = new URL('node_modules/', monacoUnified)
-
+/** @type {import('webpack').Configuration} */
 export default {
   output: {
-    filename: '[contenthash].js',
+    filename: '[contenthash].js'
   },
   resolve: {
     extensions: ['.mjs', '.js', '.ts'],
     extensionAlias: {
-      '.js': ['.js', '.ts'],
+      '.js': ['.js', '.ts']
     },
-    alias: {
-      'monaco-unified': fileURLToPath(monacoUnified),
-      'decode-named-character-reference': fileURLToPath(
-        new URL('decode-named-character-reference/index.js', nodeModules),
-      ),
-    },
+    conditionNames: ['worker']
   },
   module: {
     rules: [
       {
         test: /\.css$/,
-        use: [MiniCssExtractPlugin.loader, 'css-loader'],
+        use: [MiniCssExtractPlugin.loader, 'css-loader']
       },
       {
         // Monaco editor uses .ttf icons.
         test: /\.(svg|ttf)$/,
-        type: 'asset/resource',
+        type: 'asset/resource'
       },
       {
         test: /\.ts$/,
         loader: 'ts-loader',
-        options: { transpileOnly: true },
-      },
-    ],
+        options: { transpileOnly: true }
+      }
+    ]
   },
   optimization: {
-    minimizer: ['...', new CssMinimizerPlugin()],
+    minimizer: ['...', new CssMinimizerPlugin()]
   },
-  plugins: [new HtmlWebPackPlugin(), new MiniCssExtractPlugin({ filename: '[contenthash].css' })],
+  plugins: [new HtmlWebPackPlugin(), new MiniCssExtractPlugin({ filename: '[contenthash].css' })]
 }
